ebdc5b9c253c9bf087f56e9c4e8b74b20cd785e6
cailinguang <user_cai@126.com>
2018-04-12 17:42:04 +0800

    init

---
+++ src/main/java/com/company/project/model/Role.java
@@ -0,0 +1,71 @@
+package com.company.project.model;
+
+import javax.persistence.*;
+
+public class Role {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private String id;
+
+    private String code;
+
+    private String name;
+
+    private String description;
+
+    /**
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/company/project/service/RoleService.java
@@ -0,0 +1,11 @@
+package com.company.project.service;
+import com.company.project.model.Role;
+import com.company.project.core.Service;
+
+
+/**
+ * Created by CodeGenerator on 2018/04/12.
+ */
+public interface RoleService extends Service<Role> {
+
+}
